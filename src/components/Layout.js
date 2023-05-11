fb9b235b138e7a6c1e3eb142825cf55ec8449b9b
Ayrton Santos <ayrton.arc@gmail.com>
2023-05-10 20:11:48 -0700

    about section

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,11 +6,11 @@ import Link from "next/link";
 export default function Layout({ children }) {
   return (
     <>
-      <Header title="John Doe">
+      <Header title="Ayrton Santos">
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
-        <Link href="/about">Projects</Link>
-        <Link href="/about">Contact</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/contact">Contact</Link>
       </Header>
       <Container fixed>
         <main>{children}</main>
---
+++ src/pages/contact.js
@@ -0,0 +1,4 @@
+export default function ContactPage() {
+    return <h1>contact B</h1>;
+  }
+  
